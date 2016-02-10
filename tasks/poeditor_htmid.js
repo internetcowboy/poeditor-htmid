1bfd49da694086591dac71eb195f0fad518a1321
Codin Pangell <cpangell@hugeinc.com>
2016-02-10 15:52:57 -0800

    modified structure a bit, added promises

--- tasks/poeditor_htmid.js
+++ tasks/poeditor_htmid.js
@@ -9,29 +9,33 @@
 'use strict';
 
 var fs = require('fs'),
-	wget = require('wget'),
-	https = require('https'),
-	colors = require('colors'),
-	request = require('request'),
-	querystring = require('querystring');
+  wget = require('wget'),
+  https = require('https'),
+  colors = require('colors'),
+  request = require('request'),
+  querystring = require('querystring'),
+  done = null;
 
 var grunt;
 
+var completedLoads = 1;
+
 module.exports = function(g) {
-	
-	grunt = g;
-	grunt.registerMultiTask('poeditor_htmid',
-	'This is an adaptation of a plugin found https://github.com/Philoozushi/grunt-poeditor-pz . This plugin will download the localizations for a particular project ID and then get all of the string associated with the project. This plugin is to be used in combination with the other htmid plugins.',
-	function() {
-		var data = this.data;
-	    var opts = this.options();
-	    download( data, this, opts, this.async() );
-	});
+  
+  grunt = g;
+  grunt.registerMultiTask('poeditor_htmid',
+  'This is an adaptation of a plugin found https://github.com/Philoozushi/grunt-poeditor-pz . This plugin will download the localizations for a particular project ID and then get all of the string associated with the project. This plugin is to be used in combination with the other htmid plugins.',
+  function() {
+    done = this.async();
+
+    var data = this.data;
+      var opts = this.options();
+      download( data, this, opts );
+  });
 };
 
 function getLanguages(_scope,opts,data,_cb) {
   
-  var done = _scope.async();
   var config ={
           action: 'list_languages',
           id: opts.project_id,
@@ -41,12 +45,13 @@ function getLanguages(_scope,opts,data,_cb) {
   callAPI (config, function(res) {
     data.projectLanguages = res.list;
     grunt.log.writeln('->'.green, "Complete");
+    grunt.log.writeln(JSON.stringify(data.projectLanguages));
     _cb();
   });
 
 }
 
-function download(data, _scope, opts, done) {
+function download(data, _scope, opts) {
   
   getLanguages(_scope,opts,data, function(){
 
@@ -55,104 +60,86 @@ function download(data, _scope, opts, done) {
     data.api_token = opts.api_token;
     data.numLanguages = 0;
 
-    for (var i in data.projectLanguages)
+    for (var i in data.projectLanguages) {
       data.numLanguages++;
+    }
 
     for (var i in data.projectLanguages) {
-
-      //download file
-      callAPI({
-        api_token: data.api_token,
-        action: 'export',
-        id: data.project_id,
-        language: data.projectLanguages[i]['code'],
-        type: data.type
-      },
-      function(res, command) {
-        if (res.item)
-          exports[command.language] = res.item;
-        if (--data.numLanguages == 0) {
-          for (var polang in exports) {
-            grunt.log.writeln('->'.green, polang+':', exports[polang]);
-            downloadExports(exports, data, opts, function(paths) {
-              done();
-            });
-          }
-        }
-      });
-
+      (function(data,i) {
+        //request url of data
+        callAPI({
+          api_token: data.api_token,
+          action: 'export',
+          id: data.project_id,
+          language: data.projectLanguages[i]['code'],
+          type: data.type
+        },
+        function(res, command) {
+          if (res.item) {
+            exports[command.language] = res.item;
+            grunt.log.writeln('->'.green, command.language+':', exports[command.language]);
+            downloadExports(_scope, command.language, res.item, data, opts);
+          } 
+        }); 
+      })( data,i );
     }
+
   });
 
 }
 
-function downloadExports(exports, data, opts, handler) {
-  
-  var numDownloads = 0;
-  for (var polang in exports)
-    numDownloads++;
-  
-  var paths = [];
-  for (var polang in exports) {
-    
-    var url = exports[polang];
-    var lang = data.projectLanguages[polang];
-    var path = data.dest.replace('?', lang);
-    
-    //add file name
-    paths.push(path);
 
-    //create closure to pass variables into scope we need.
-    (function(_url,_path,_polang) {
-          request(_url, function(err, res, body) {
-              
-              //just save the stuff we want in the format we want.
-        var json = JSON.parse(body);
-        var jsonData = {};
-        var webfilename = "";
-        for (var item in json) {
-          var terms = json[item];
+function downloadExports(_scope, polang, url, data, opts) {
+    
+  var lang = data.projectLanguages[polang];
+  var path = data.dest.replace('?', lang);
+
+  //create closure to pass variables into scope we need.
+  (function(_url,_path,_polang,_numLanguages) {
+      request(_url, function(err, res, body) {
           
-          //parse file name out (website file name). _term is something like this: 'index_ Title'
-          var _term = terms.term;
-          var webfilename = _term.split(" ")[0];
-          _term = _term.split(" ")[1];
-          webfilename = webfilename.split("_").join("");
+          //just save the stuff we want in the format we want.
+          var json = JSON.parse(body);
+          var jsonData = {};
+          var webfilename = "";
+          for (var item in json) {
+            var terms = json[item];
+            
+            //parse file name out (website file name). _term is something like this: 'index_ Title'
+            var _term = terms.term;
+            var webfilename = _term.split(" ")[0];
+            _term = _term.split(" ")[1];
+            webfilename = webfilename.split("_").join("");
 
-          //add data to object
-          jsonData[_term] = terms.definition;
-        }
-
-        //determine if the localization name has a force replacement.
-        var _name = _polang;
-        for (var key in opts.rewriteLangs) {
-          if (key == _name) {
-            _name = opts.rewriteLangs[key];
+            //add data to object
+            jsonData[_term] = terms.definition;
           }
-        }
-        //replace all dashes with underscores
-        _name = _name.split("-").join("_");
 
-        //save this data in place of the downloaded file. index--en_gb.json
-        _path = _path + webfilename + "--" + _name.toLowerCase() + ".json";
-        grunt.file.write(_path, JSON.stringify(jsonData));
+          //determine if the localization name has a force replacement.
+          var _name = _polang;
+          for (var key in opts.rewriteLangs) {
+            if (key == _name) {
+              _name = opts.rewriteLangs[key];
+            }
+          }
+          //replace all dashes with underscores
+          _name = _name.split("-").join("_");
 
-        //keep track of completion
-        if (--numDownloads == 0)
-        handler(paths);
+          //save this data in place of the downloaded file. index--en_gb.json
+          _path = _path + webfilename + "--" + _name.toLowerCase() + ".json";
+          grunt.file.write(_path, JSON.stringify(jsonData));
 
-          });
-      })( url, path, polang );
+          //determine when last item has been downloaded
+          if (_numLanguages==completedLoads) {
+            grunt.log.writeln("Downloads Complete");
+            done();
+            return;
+          }
 
-  }
-}
+          completedLoads++;
+      });
+  })( url, path, polang, data.numLanguages );
 
-function downloadExport(url, path, handler) {
-  
-  wget.download(url, path)
-    .on('end', function(output) {
-      handler(path,output);
-    });
 }
 
 function callAPI(command, handler) {
